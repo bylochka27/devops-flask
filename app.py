6fde5bf083efb9e99937e4ea3327b2c2a350cb00
bylochka27 <andrejlytaev11@gmail.com>
2025-08-08 20:29:25 +0300

    Add/update CI workflow, app.py and Docker files

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	return "Hello , devops from Flask"
+@app.route("/hello/<name>/<age>")
+def hello(name,age):
+	return f"Привет, {name}! Тебе {age} лет."
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+	app.run(host="0.0.0.0", port=5000,debug =True)
